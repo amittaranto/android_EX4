844cd975aad19ba0bc32a92c1ea004961bedf268
liorliord <liorliord@gmail.com>
2019-06-15 00:12:00 +0300

    Aadding donate page and design

--- app/src/main/java/com/example/ptakim/donatePage.java
+++ app/src/main/java/com/example/ptakim/donatePage.java
@@ -1,9 +1,12 @@
 package com.example.ptakim;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class donatePage extends AppCompatActivity {
 
@@ -14,11 +17,17 @@ public class donatePage extends AppCompatActivity {
     }
 
     public void add_money(View view) {
-        EditText et =  findViewById(R.id.donateInput);
+        SharedPreferences sp = getSharedPreferences("donations", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        TextView et =  findViewById(R.id.donateInput);
+        TextView tv = findViewById(R.id.donationOverall);
         String donation = et.getText().toString();
         try {
-            double d = Double.parseDouble(donation);
-
+            float d = Float.parseFloat(donation);
+            d += sp.getFloat("donations",0);
+            editor.putFloat("donations", d);
+            editor.apply();
+            tv.setText(Float.toString(sp.getFloat("donations",0)));
         } catch (NumberFormatException | NullPointerException nfe) {
             return;
         }
